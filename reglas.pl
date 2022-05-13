7224862e99cd5271c03f049432a0250c29066c0b
Dracko <joseph13rios@hotmail.com>
2022-05-12 20:23:12 -0500

    Primera regla

--- reglas.pl
+++ reglas.pl
@@ -11,3 +11,9 @@ tematica(timing_bueno, tematica_banda_rock).
 tematica(timing_bueno, tematica_naves_espaciales).
 
 juego_recomendado(tematica_banda_rock, 'Rock Band').
+
+%Reglas del arbol
+recomendacion(A,B,C,D,R):-forma_jugar(A),reflejos(A,B),timing(B, C),tematica(C, D),write('El juego recomendado es:'),juego_recomendado(C,X).
+
+%preguntas
+%recomendacion(forma_jugar_casual,reflejos_buenos,timing_bueno,tematica_banda_rock,R).
